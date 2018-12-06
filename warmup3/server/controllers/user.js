b9a5c87fb608025b02abfdbcf1083c642f253204
esadeli <efrat.sadeli@gmail.com>
2018-12-06 19:10:38 +0700

    User Login

--- warmup3/server/controllers/user.js
+++ warmup3/server/controllers/user.js
@@ -3,6 +3,7 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+// const hashPassword = require('../helpers/hashPassword')
 
 module.exports = {
   register: function(req,res) {
@@ -39,6 +40,28 @@ module.exports = {
       })
   },
   login: function (req,res) {
-    
+    User.findOne({
+      email: req.body.email
+    })
+      .then(user => {
+        console.log('user login ----', user)
+        // check if password and hash is verified
+        if(bcrypt.compareSync(req.body.password, user.password)){
+          // get the jwt token
+          jwt.sign({
+            userid: user._id,
+            name: user.name,
+            email: user.email
+          }, process.env.SECRET_TOKEN, (err,token)=> {
+            
+          })
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          msg: 'Error user login',
+          err: error
+        })
+      })
   }
 }
