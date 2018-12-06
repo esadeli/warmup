a3bb63786ead47dc73120512c80ea275775056fe
esadeli <efrat.sadeli@gmail.com>
2018-12-06 16:27:49 +0700

    Fibbonaci OK

---
+++ warmup2/fibonacci.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Fibonacci series
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+function fibbonaci(input) {
+    
+    // suku ke 1 dan ke 2
+    if (input === 1){
+        return 0
+    } else if (input === 2){
+        return 1
+    }
+
+    return fibbonaci(input-1) + fibbonaci(input-2)
+}
+
+// testing 
+console.log(fibbonaci(2)) // 1
+console.log(fibbonaci(5)) // 3
+console.log(fibbonaci(9)) // 21
---
+++ warmup2/readme.md
@@ -0,0 +1,2 @@
+# Challenge 2
+##### 0, 1, 1, 2, 3, 5, 8, 13 dst adalah deret fibonacci Buatlah fungsi dalam javascript yang mengambil deret fibonacci ke N
