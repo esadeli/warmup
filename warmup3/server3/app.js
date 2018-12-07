57988086f7b4858ca4b427d3dc925443218a2bee
esadeli <efrat.sadeli@gmail.com>
2018-12-07 09:59:53 +0700

    Initial set up

---
+++ warmup3/server3/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/', (req,res) => { res.status(200).json({msg: 'Main Server OK'})})
+app.listen(process.env.PORT || 3000, ()=> { console.log(`Listening to PORT ${process.env.PORT}`)})
