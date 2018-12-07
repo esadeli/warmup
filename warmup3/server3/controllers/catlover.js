2a60f01a6cc03eeede039d5180b804b258f95975
esadeli <efrat.sadeli@gmail.com>
2018-12-07 12:41:37 +0700

    Catlover CRUD OK

--- warmup3/server3/controllers/catlover.js
+++ warmup3/server3/controllers/catlover.js
@@ -40,5 +40,57 @@ module.exports = {
       .catch(err => {
         res.status(500).json(err.response.data)
       })
+  },
+  get: function (req,res) {
+    axios({
+      method: 'GET',
+      url: `http://localhost:5021/user/${req.params.id}`,
+      headers: {
+        serverkey: process.env.SERVERKEY
+      }
+    })
+      .then(user => {
+        res.status(201).json(user.data)
+      })
+      .catch(err => {
+        res.status(500).json(err.response.data)
+      })
+  },
+  update: function (req,res) {
+    axios({
+      method: 'PUT',
+      url: `http://localhost:5021/user/${req.params.id}`,
+      headers: {
+        serverkey: process.env.SERVERKEY
+      },
+      data: {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        address: req.body.address,
+        city: req.body.city,
+        zip: req.body.zip
+      }
+    })
+      .then(user => {
+        res.status(201).json(user.data)
+      })
+      .catch(err => {
+        res.status(500).json(err.response.data)
+      })
+  },
+  remove: function (req,res) {
+    axios({
+      method: 'DELETE',
+      url: `http://localhost:5021/user/${req.params.id}`,
+      headers: {
+        serverkey: process.env.SERVERKEY
+      }
+    })
+      .then(user => {
+        res.status(201).json(user.data)
+      })
+      .catch(err => {
+        res.status(500).json(err.response.data)
+      })
   }
 }
--- warmup3/server3/routes/CatLoverRoutes.js
+++ warmup3/server3/routes/CatLoverRoutes.js
@@ -3,9 +3,12 @@
 const express = require('express')
 const router = express.Router()
 const CatLoverController = require('../controllers/catlover')
-const { add, list} = CatLoverController
+const { add, list, get, update, remove} = CatLoverController
 
 router.post('/', add)
       .get('/list', list)
+      .get('/:id', get)
+      .put('/:id', update)
+      .delete('/:id', remove )
 
 module.exports = router
